8d7008025e83d9ca37203aed84111d712ae55c3e
ps5562 <ps5562@live.ca>
2026-03-08 20:40:27 -0400

    Implement scoring system and display scores in Pong game

--- main.py
+++ main.py
@@ -9,11 +9,13 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((800, 600))
     clock = pygame.time.Clock()
+    font = pygame.font.Font(None, 36)
 
     # Game objects using classes
     ball = Ball(400, 300)
     player1 = Paddle(50, 250)
     player2 = Paddle(740, 250)
+    game_controller = GameController()
 
     while True:
         for event in pygame.event.get():
@@ -27,6 +29,11 @@ def main():
         # Ball collision with paddles
         if ball.rect.colliderect(player1.rect) or ball.rect.colliderect(player2.rect):
             ball.speed_x *= -1
+
+        # Check for scoring
+        scorer = game_controller.check_score(ball)
+        if scorer != 0:
+            game_controller.reset_ball(ball)
 
         # Move player1 (human)
         keys = pygame.key.get_pressed()
@@ -43,6 +50,13 @@ def main():
         player1.draw(screen)
         player2.draw(screen)
         ball.draw(screen)
+        
+        # Draw scores
+        score1_text = font.render(str(game_controller.player1_score), True, (255, 255, 255))
+        score2_text = font.render(str(game_controller.player2_score), True, (255, 255, 255))
+        screen.blit(score1_text, (100, 50))
+        screen.blit(score2_text, (700, 50))
+        
         pygame.display.flip()
         clock.tick(60)
 
@@ -55,6 +69,36 @@ def ai_move(player2, ball):
         player2.y += 5
     elif player2.centery > ball.centery and player2.top > 0:
         player2.y -= 5
+
+# Game Controller class
+class GameController:
+    """
+    Manages game state including score tracking.
+    """
+    def __init__(self):
+        self.player1_score = 0
+        self.player2_score = 0
+    
+    def check_score(self, ball):
+        """
+        Check if a point has been scored and return the player who scored.
+        Returns 1 if player1 scored, 2 if player2 scored, 0 if no score.
+        """
+        if ball.rect.left < 0:
+            self.player2_score += 1
+            return 2
+        elif ball.rect.right > 800:
+            self.player1_score += 1
+            return 1
+        return 0
+    
+    def reset_ball(self, ball):
+        """
+        Reset the ball to the center of the screen.
+        """
+        ball.rect.center = (400, 300)
+        ball.speed_x = 5
+        ball.speed_y = 5
 
 # Paddle class
 class Paddle:
